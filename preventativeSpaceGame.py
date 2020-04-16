0885f8826ceb429fe6ac0b8138129a0e43f895f5
Taylor Nguyen <taylor.nguyen18@yahoo.com>
2020-04-16 12:53:56 -0700

    updated

--- preventativeSpaceGame.py
+++ preventativeSpaceGame.py
@@ -1,38 +1,5 @@
 import time
 import random
-'''
-from twilio.rest import Client
-
-account_sid = 'AC751f6b64a9a84b96c57142d7eaccb4ad'
-auth_token = 'c87adf9fa0af841e83f16729928c5839'
-client = Client(account_sid, auth_token)
-
-{
-  "account_sid": "AC751f6b64a9a84b96c57142d7eaccb4ad",
-  "api_version": "2010-04-01",
-  "body": "Join Earth's mightiest heroes. Like Kevin Bacon.",
-  "date_created": "Thu, 30 Jul 2015 20:12:31 +0000",
-  "date_sent": "Thu, 30 Jul 2015 20:12:33 +0000",
-  "date_updated": "Thu, 30 Jul 2015 20:12:33 +0000",
-  "direction": "outbound-api",
-  "error_code": null,
-  "error_message": null,
-  "from": "+15017122661",
-  "messaging_service_sid": "MGXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
-  "num_media": "0",
-  "num_segments": "1",
-  "price": null,
-  "price_unit": null,
-  "sid": "MMXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
-  "status": "sent",
-  "subresource_uris": {
-    "media": "/2010-04-01/Accounts/ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX/Messages/SMXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX/Media.json"
-  },
-  "to": "+15558675310",
-  "uri": "/2010-04-01/Accounts/ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX/Messages/SMXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX.json"
-} 
-
-'''
 
 #myGame = Game(newName='hello')
 
